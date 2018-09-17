cf520646eb8201c6e4a14ca925ddc5edc9fcf942
German <gferrari@antel.com.uy>
2018-09-17 09:34:37 -0300

    java TERMINAL standalone

---
+++ TERMINAL/src/main/java/uy/com/antel/App.java
@@ -0,0 +1,13 @@
+package uy.com.antel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
