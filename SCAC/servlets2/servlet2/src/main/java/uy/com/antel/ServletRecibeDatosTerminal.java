6bd3f2f42d4e55de3f7fd5935eab9477167c64c2
German <gferrari@antel.com.uy>
2018-09-24 08:17:15 -0300

    Se le indica al Servlet que arranque con el servidor de aplicaciones

--- SCAC/servlets2/servlet2/src/main/java/uy/com/antel/ServletRecibeDatosTerminal.java
+++ SCAC/servlets2/servlet2/src/main/java/uy/com/antel/ServletRecibeDatosTerminal.java
@@ -1,8 +1,10 @@
 package uy.com.antel;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+@WebServlet( urlPatterns = {"/MiServlet"}, loadOnStartup = 1)
 public class ServletRecibeDatosTerminal extends HttpServlet {
 
     @Override
